01efbbe2efbfcc4e2aaeb8e9428aee8144e76596
Ferhan Akkan <ferhanakkan@gmail.com>
2020-04-08 23:12:23 +0300

    İnitial

--- ApiCallWithAlamofire/ViewController.swift
+++ ApiCallWithAlamofire/ViewController.swift
@@ -7,13 +7,60 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let baseUrl = "https://jsonplaceholder.typicode.com"
+        
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        switch sender.currentTitle! {
+        case "Get":
+            AF.request("\(baseUrl)/posts", method: .get).responseJSON { response in
+                if let error = response.error {
+                    print(error)
+                } else {
+                    print(response)
+                }
+            }
+            
+        case "Post":
+            let params:[String : Any] = ["title":"TestPost", "body":"TestPost2", "userId":22]
+            
+            AF.request("\(baseUrl)/posts", method: .post, parameters: params).responseJSON { response in
+                if let error = response.error {
+                    print(error)
+                } else {
+                    print(response)
+                }
+            }
+            
+            
+        case "Put":
+            let params:[String : Any] = ["title":"TestPut", "body":"TestPut2", "userId":40]
+
+            AF.request("\(baseUrl)/posts/1", method: .put, parameters: params).responseJSON { response in
+                if let error = response.error {
+                    print(error)
+                } else {
+                    print(response)
+                }
+            }
+            
+            
+        case "Delete":
+            AF.request("\(baseUrl)/posts/1", method: .delete).responseJSON { response in
+                if let error = response.error {
+                    print(error)
+                } else {
+                    print(response)
+                }
+            }
+        default:
+            return
+        }
     }
+    
 
 
 }
